6be073b8e312e3abbd5037b5c5aefc48b130d1d9
TheikACR <davidinfante1997@gmail.com>
2018-05-16 16:06:42 +0200

    bug fixes

--- fps/avatar.js
+++ fps/avatar.js
@@ -113,23 +113,23 @@ class Avatar {
     animateWeapon() {
         if (this.activeWeapon == 0) {
             if (this.recoil) {
-                if (this.camera.children[1].rotation.x >= 0.2) this.recoil = false;
-                else this.camera.children[1].rotation.x += 0.1;
+                if (this.rifle.rotation.x >= 0.2) this.recoil = false;
+                else this.rifle.rotation.x += 0.1;
             } else {
-                if (this.camera.children[1].rotation.x >= 0 && this.camera.children[1].rotation.x <= 0.1) {
+                if (this.rifle.rotation.x >= 0 && this.rifle.rotation.x <= 0.1) {
                     disparando = false;
                     this.recoil = true;
-                } else this.camera.children[1].rotation.x -= 0.1;
+                } else this.rifle.rotation.x -= 0.1;
             }
         } else if (this.activeWeapon == 1) {
             if (this.recoil) {
-                if (this.camera.children[2].rotation.x >= 1.8) this.recoil = false;
-                else this.camera.children[2].rotation.x += 0.1;
+                if (this.shotgun.rotation.x >= 1.8) this.recoil = false;
+                else this.shotgun.rotation.x += 0.1;
             } else {
-                if (this.camera.children[2].rotation.x >= 0 && this.camera.children[2].rotation.x <= 0.1) {
+                if (this.shotgun.rotation.x >= 0 && this.shotgun.rotation.x <= 0.1) {
                     disparando = false;
                     this.recoil = true;
-                } else this.camera.children[2].rotation.x -= 0.1;
+                } else this.shotgun.rotation.x -= 0.1;
             }
         }
     }
